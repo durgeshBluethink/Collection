fe6755a1e71b4de5eedf358b3fbd620cb46990ea
durgeshbluethunk <durgesh1.bluethink@gmail.com>
2022-12-15 19:50:10 +0530

    type change in test file

--- src/test.java
+++ src/test.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class test {
     public static void main(String[] args) {
-        ArrayList<String > names= new ArrayList<>();
+        ArrayList<Integer > names= new ArrayList<>();
         for (int i = 0; i <= names.size(); i++) {
           names.add(i);
           System.out.println(names);
